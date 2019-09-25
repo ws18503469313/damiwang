bff4abc89ebe903f4e194225f03307763e4ea981
polunzi <849307440@qq.com>
2019-09-25 20:50:00 +0800

    slueth&&zipkin 链路追踪

--- sleuth-server/src/main/java/com/cloud/provide/ZipkinApplication.java
+++ sleuth-server/src/main/java/com/cloud/provide/ZipkinApplication.java
@@ -2,9 +2,11 @@ package com.cloud.provide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZipkinServer
 public class ZipkinApplication {
 
